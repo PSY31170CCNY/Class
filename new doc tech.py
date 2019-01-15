8d5acd676658cb5f7546d905ce08f7ad9e826c5c
sashagay456 <46355967+sashagay456@users.noreply.github.com>
2019-01-15 17:07:20 -0500

    Aleshia

---
+++ new doc tech.py
@@ -0,0 +1,44 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:44:40) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+>>> ================================ RESTART ================================
+>>> 
+enter the name and the email address
+input first name Leshia
+input last name Thompson
+input email address sashagay456@yahoo.com
+>>> ================================ RESTART ================================
+>>> 
+enter the name and the email address
+input first name Aleshia
+input last name Stewart
+input email address sashagay456@yahoo.com
+Traceback (most recent call last):
+  File "E:/Technology Class 1.py", line 29, in <module>
+    if self.firstname =="" :
+NameError: name 'self' is not defined
+>>> entry
+<__main__.Person object at 0x0000000003AC3470>
+>>> entry.firstname
+' Aleshia'
+>>> ================================ RESTART ================================
+>>> 
+enter the name and the email address
+input first name Aleshia
+input last name Stewart
+input email address sashagay456@yahoo.com
+enter the name and the email address
+input first name Aleshia
+input last name Stewart
+input email address sashagay456@yahoo.com
+enter the name and the email address
+input first name Aleshia
+input last name Stewart
+input email address sashagay456@yahoo.com
+enter the name and the email address
+input first name Aleshia
+input last name Stewart
+input email address sashagay456@yahoo.com
+enter the name and the email address
+input first name
