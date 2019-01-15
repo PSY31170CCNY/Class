ecb48ada32a8bdf43938f57114f4d11a6969d329
TobiKolawole <46355483+TobiKolawole@users.noreply.github.com>
2019-01-15 17:03:33 -0500

    Add files via upload

---
+++ Oluwatobi Kolawole/Problem 1.py
@@ -0,0 +1,15 @@
+A = input("What is your first name?")
+B =input("What is your last name?")
+C = input("What is your email address?")
+print(A , B , C )
+
+
+
+drive='/Users/okolawo000/Desktop/'
+e=open(drive+'contactlist.csv','w')
+while A is not A:
+    A2= input("What is your new first name?")
+    print(A2)
+
+e.close()                  
+    
