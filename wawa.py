963af960265ec84cd9ac7dcbfe3b6b76ca68fa10
davebritton <dave@davebritton.com>
2019-01-15 11:06:44 -0500

    Delete wawa.py

--- wawa.py
+++
@@ -1,9 +0,0 @@
-"""display: heading
-   menu choices:
-      - add a new entry
-      - select or find an entry to edit
-      - create a cav file
-
-the program:
-     1. define what an entry consist of
-       - information content ==> list of proplr
