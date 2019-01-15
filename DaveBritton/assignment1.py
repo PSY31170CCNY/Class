0ea185e02f4df00cf77a6dd4b6bb83dc1234d20b
davebritton <dave@davebritton.com>
2019-01-15 13:53:30 -0500

    assignment 1
    example of how to start

---
+++ DaveBritton/assignment1.py
@@ -0,0 +1,7 @@
+# assignment1.py
+# Dave Britton
+#create a user-entered csv list of names and email addresses
+morenames=True
+while morenames:
+    # prompt the user to enter the firstname, lastname and email address
+    # 
