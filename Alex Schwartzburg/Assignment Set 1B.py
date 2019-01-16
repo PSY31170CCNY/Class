5aec8841fb081348d1d39ac407d87d105d620fe6
YoungMan2019 <aschwartzburg@gmail.com>
2019-01-16 13:56:49 -0500

    #Assignment Set 1B (updated 1/16/2019)
    - Cleaned
    - Added file location functionality

--- Alex Schwartzburg/Assignment Set 1B.py
+++ Alex Schwartzburg/Assignment Set 1B.py
@@ -1,25 +1,45 @@
-#Assignment Set 1B
+#Assignment Set 1B (updated 1/16/2019)
 
-DatabaseExtension = "C:/Users/aschwar002/Desktop/01.15.2019/"
-DatabaseFilename = "email list02.csv"
+#Variable assignments
+##First Choice
+var1 = input("Would you like to create a new file or use the default? \n1 - Use Default.\n2 - Create new file.")
+#if var1 = 1, use default.
+while var1 == "":
+    var1 = input("Would you like to create a new file or use the default? \n1 - Use Default.\n2 - Create new file.")
+else:
+    if var1 == '1':
+        Extension = "C:/Users/aschwar002/Desktop/"
+        Filename = "FormLetter.txt"
+    elif var1 == '2':
+        Extension = input("Where would you like to create your file?\n###Enter the file path.")
+        Filename = input("What would you like to call this file?\n###Enter the filename.")
 
-Extension = "C:/Users/aschwar002/Desktop/01.15.2019/"#input("Where would you like to create your file?\n###Enter the file path.")
-Filename = "FormLetter.txt" #input("What would you like to call this file?\n###Enter the filename.")
-##Contents = input("What would you like the file to say?")
+##2nd Choice - which database do you want to use?
+var2 = input("Would you like to use the default database or call a new one? \n1 - Use Default.\n2 - Create new file.")
+###if var2 = 1, use default.
+while var2 == "":
+    var2 = input("Would you like to use the default database or call a new one? \n1 - Use Default.\n2 - Create new file.")
+else:
+    if var2 == '1':
+        DatabaseExtension = "C:/Users/aschwar002/Desktop/"
+        DatabaseFilename = "email_list.csv"
+    elif var2 == '2':
+        DatabaseExtension = input("Where is the database?\n###Enter a file path.")
+        DatabaseFilename = input("What would you like to call this file?\n###Enter a filename.")
+
+##3rd variable
 File = Extension + Filename
 
-###Grabbing their name
-##name = input("Who are you contacting?\n###Enter their name.")
-##
-###Grabbing their email
-##email = input("What is their email?\n###Enter their email.")
 
+#Code
 e = open(DatabaseExtension+DatabaseFilename,'r')
-namelist = e.readlines()
+NameList = e.readlines()
 fieldnames = ["firstname","lastname","email"]
 e.close()
-mergeList=[]
-for name in namelist:
+
+mergeList = []
+
+for name in NameList:
     fields = name.split(',')
     print(fields)
     n=0
@@ -29,16 +49,11 @@ for name in namelist:
         names[fieldnames[n]] = item
         n=n+1
     mergeList.append(names)
-print("Printnames")
 print(names)
 
 f = open(File,'a')
 for x in mergeList:
-    var01 = "Dear "+x["firstname"]+" "+x["lastname"]+',Is this your correct email address? '+x["email"]+"\n"
-    print(var01)
-    f.writelines(var01)
+    var02 = "Dear "+x["firstname"]+" "+x["lastname"]+',Is this your correct email address? '+x["email"]+"\n"
+    print(var02)
+    f.writelines(var02)
 f.close()
-
-
-
-
