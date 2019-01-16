d702f0e257220c14e181c2e0b3116b1b666d5bcb
TobiKolawole <46355483+TobiKolawole@users.noreply.github.com>
2019-01-16 12:57:13 -0500

    Update Problem 1.py

--- Oluwatobi Kolawole/Problem 1.py
+++ Oluwatobi Kolawole/Problem 1.py
@@ -1,15 +1,19 @@
-A = input("What is your first name?")
-B =input("What is your last name?")
-C = input("What is your email address?")
-print(A , B , C )
-
-
-
 drive='/Users/okolawo000/Desktop/'
 e=open(drive+'contactlist.csv','w')
-while A is not A:
-    A2= input("What is your new first name?")
-    print(A2)
-
-e.close()                  
+Rename = True
+while Rename:
+    Info = input("Would you like to insert a new person? Y/N")
+  
+    while Info in "Yy/YES/yes":
+        A= input("What is your first name?")
+        B = input("What is your last name?")
+        C= input("Enter your email address.")
+        
+        Data = str('"'+ A +'",'+'"'+ B +'",'+ C +'\n')
+        
+        
+        e.writelines(Data)
+        e.close()
+        continue
+    print("Try again. Please try again")               
     
