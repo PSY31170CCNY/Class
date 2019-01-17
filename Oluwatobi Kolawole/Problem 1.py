702c80c7ea440dedca6528e8fd671bbdff5b55fe
TobiKolawole <46355483+TobiKolawole@users.noreply.github.com>
2019-01-17 13:26:30 -0500

    Update Problem 1.py

--- Oluwatobi Kolawole/Problem 1.py
+++ Oluwatobi Kolawole/Problem 1.py
@@ -1,19 +1,2 @@
-drive='/Users/okolawo000/Desktop/'
-e=open(drive+'contactlist.csv','w')
-Rename = True
-while Rename:
-    Info = input("Would you like to insert a new person? Y/N")
-  
-    while Info in "Yy/YES/yes":
-        A= input("What is your first name?")
-        B = input("What is your last name?")
-        C= input("Enter your email address.")
-        
-        Data = str('"'+ A +'",'+'"'+ B +'",'+ C +'\n')
-        
-        
-        e.writelines(Data)
-        e.close()
-        continue
-    print("Try again. Please try again")               
+       
     
