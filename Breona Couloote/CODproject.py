ae7617c2e394c4906ea6bc2a97f0ea1056bf7549
bcouloote <46355511+bcouloote@users.noreply.github.com>
2019-01-18 16:51:35 -0500

    Add files via upload

---
+++ Breona Couloote/CODproject.py
@@ -0,0 +1,19 @@
+#classprojectCOD
+import csv
+
+year = 1
+cause = 2
+sex = 3
+race = 4
+death = 5
+dr = 6 #death rate
+age = 7
+
+class COD
+    def __init__(self,year='', cause='', sex='', race='',death='',dr='',age='')
+        self.year=year
+        self.cause=cause
+        self.sex=sex
+        self.race=race
+        self.death=death
+
