ec5f9a27db1a3f363c4e945395fad6ba84eb7211
justin-pearce <justinpearce231@gmail.com>
2019-01-23 14:39:34 -0500

    Update ex.py

--- Justin Pearce/ex.py
+++ Justin Pearce/ex.py
@@ -12,3 +12,4 @@ Counter = Counter(split_it)
 most_occur = Counter.most_common(4) 
   
 print(most_occur) 
+# Using python I analyzed the data based on the most used location where accidents happen in the list. The data was on nyc motor vehicle  collisions. Based on the data and code I concluded that most motor vehicle accidents happened  in Brooklyn 22 accidents followed by Queens with 15 accidents bronx had 7 and Manhattan had 6.
