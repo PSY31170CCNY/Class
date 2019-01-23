fcf37993a125784a127e0de36e0b6b2266ca1dd4
bcouloote <46355511+bcouloote@users.noreply.github.com>
2019-01-23 16:05:43 -0500

    Add files via upload

--- Breona Couloote/CODproject.py
+++ Breona Couloote/CODproject.py
@@ -1,17 +1,21 @@
 #classprojectCOD
+#the files are looking at New York City Leading Cause of Death
+#my project is comparing males andvfemales leading cause of death 
 import csv
-
-uid=0
-year = 1
-cause = 2
+#link:
+#https://data.cityofnewyork.us/Health/New-York-City-Leading-Causes-of-Death/jb7j-dtam 
+uid=0 #unique id 
+year = 1 #year of death
+cause = 2 #cause of death
 sex = 3
 race = 4
-death = 5
+death = 5 #number of deaths
 dr = 6 #death rate
-age = 7
-
+age = 7 #age rate
+#making a class to hold the datawhat is in the excel file
 class COD:
-    def __init__(self,uid='',year='', cause='', sex='', race='',death='',dr='',age=''):
+    def __init__(self,uniqueid='',year='', cause='', sex='', race='',death='',\
+                 dr='',age=''):
         self.uniqueid=uniqueid
         self.year=year
         self.cause=cause
@@ -21,29 +25,28 @@ class COD:
         self.dr=dr
         self.age=age
 
-hispanic = {}
-black = {}
+Female= {}
+Male= {}
 datafile = "New_York_City_Leading_Causes_of_Death.csv"
 
-with open(datafile,newline='') as csvfile:
+with open(datafile, newline='') as csvfile:
     datareader = csv.reader(csvfile,delimiter = ',', quotechar='|')
     for row in datareader:
-        lc = row[race].lower()
-        if not(('hispanic' in lc) or ('black' in lc)):
+        lc = row[sex].lower()
+        if not(('female' in lc) or ('male' in lc)):
             continue
 
     c = COD(row[year],row[uid],row[cause],' '.join([row[sex],row[race],row[death],row[dr],row[age]])
+if lc in 'sex': #im getting an invalid syntax error. I've tried everything to fix not sure why the error is happening
+            Female[row[uid]]=c
+else:
+            Male[row[uid]]=c
 
-    if lc in 'hispanic':
-        hispanic[row[uid]]=c
-    else:
-         black[row[uid]]=c
+Femcount=len(Female)
+Malcount=len(Male)
 
-hiscount = len(hispanic)
-blackcount = len(black)
-
-hiskeys = hispanic.keys()
-blackkeys = black.keys()
+femkeys = female.keys()
+malkeys = male.keys()
 
 def causecount(coddict,cause=2):
     clist=coddict.keys()
@@ -57,12 +60,11 @@ def causecount(coddict,cause=2):
             print(cause,str(row.uniqueid))
             causedict[cause]=1
     return gradedict
-
-Hiscause = causecount(hispanic,cause)
-Blackcause = causecount(black,cause)
-for s in 'ABCZ':
-    print('black cause ',s ,Hiscause[s])
-    print('hispanic cause ',s ,Blackcause[s])
+femcause = causecount(Female,cause)
+malcause = causecount(Male,cause)
+for d in 'ABCZ':
+    print('female cause ',d ,femcause[d])
+    print('male cause ',d ,malcause[d])
     
 
                 
