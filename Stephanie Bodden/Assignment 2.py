4185011e39b3c8194551e2ce5ee48cd002ea86fb
stephaniebodden <46355818+stephaniebodden@users.noreply.github.com>
2019-01-25 13:39:44 -0500

    Add files via upload

---
+++ Stephanie Bodden/Assignment 2.py
@@ -0,0 +1,26 @@
+cards= []
+suits=["Hearts", "Spades", "Diamonds", "Clubs"]
+numbers=["Ace","King","Queen","Jack","10","9","8","7","6","5","4","4","3","2","Ace"]
+for suit in range(len(suits)):
+    for num in range(len(numbers)):
+        cards.append((numbers[num]," of ", suits[suit]))
+
+random.shuffle(cards)
+
+gameover = False
+startcard = 0
+while not gameover:
+       playerhand = cards[startcard:startcard+2]
+       startcard = startcard + 1 # or +=1
+       print(playerhand)
+       hitme='x'
+       stop='a'
+       while hitme not in ["YyNn"]:
+          hitme = input("Another card? Y/N")
+# we have a valid user input now
+# is it hit me or stay?
+          if hitme in "Yy":
+              playerhand.append(cards[startcard])
+              startcard += 1  # or startcard = startcard + 1
+              print(playerhand)
+          
