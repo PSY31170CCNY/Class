f2c2be407c1a37c9d298a6a625f41e76266b0cf3
sashagay456 <46355967+sashagay456@users.noreply.github.com>
2019-01-25 23:23:52 -0500

    Twitter

---
+++ Aleshia Stewart/twitter 2.py
@@ -0,0 +1,39 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/Tiffany/AppData/Local/Programs/Python/Python37-32/Twitter 1.py 
+Thanks
+to
+the
+historic
+TAX
+CUTS
+that
+I
+signed
+into
+law,
+your
+paychecks
+are
+going
+way
+UP,
+your
+taxes
+are
+going
+way
+DOWN,
+and
+America
+is
+once
+again
+OPEN
+FOR
+BUSINESS!
+There are 3 good words in this tweet
+There are 1 bad words in this tweet
+Go back to Puerto Rico
+>>> 
