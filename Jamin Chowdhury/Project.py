95a61c345d707e906eec2d403bfc1e63ea1d4605
Jamin Chowdhury <46355702+coderjamin@users.noreply.github.com>
2019-01-25 19:35:06 -0500

    Add files via upload
    Final Project ((With Notes))

--- Jamin Chowdhury/Project.py
+++ Jamin Chowdhury/Project.py
@@ -1,27 +1,37 @@
+#Final Project
+#Locate spreadsheet
+
 import csv
 Destination = "C:/Users/Jamin/Desktop/project/"
 Filename = "Drinking_Fountains.csv"
 File = Destination + Filename
+#-------------------------------------------------
+#Label Columns
 
 num = 0  #Property Number
 name = 1  #Site Name
 boro = 2  #Borough
 drink = 3  #Fountains
-
+#--------------------------------------------------
+#Open up the spreadsheet and read it 
 with open(File, newline='') as csvfile:
     Filereader = csv.reader(csvfile,delimiter = ',', quotechar='|')
     park = {}
     for row in Filereader:
         PropertyNumber[row[1]]= Parks(row[0],row[1],row[2],row[3])
         PropertyNumber[row[0]]={'SitName':row[1],'Borough':row[2],'Fountains':row[3]}
-        
+#---------------------------------------------------
+#Put together the information
 class Parks:
     def __init__ (self, num='', name='', boro='',drink =''):
         self.num = PropertyNumber
         self.name = SiteName
         self.boro = Borough
         self.drink = Fountains
-        
+#--------------------------------------------------
+#Answer the question "Which borough has the most parks? and
+# "Which borough has the most fountains?"
+
 FountainList=restdict.keys()
     ParkListdict={}
     for P in FountainList:
@@ -34,5 +44,5 @@ FountainList=restdict.keys()
             ParkListdict[ParkList]=1 
     return ParkListdict
 
-
+#---------------------------------------------------
    
